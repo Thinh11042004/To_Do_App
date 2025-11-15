90726ab4af6c09d3cac288124f64c9e01a6b14f2
Thinh <ntt112004h@gmail.com>
2025-11-15 21:18:41 +0700

    Renew Project (Fix exposed key)

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -52,11 +53,13 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDwrqmq73PxaYu9sFfHHeu3vBteLF9GYok',
-    appId: '1:845469238451:android:10e6ac91bf35ea93e8dcc6',
-    messagingSenderId: '845469238451',
-    projectId: 'authtodoapp-b21b9',
-    storageBucket: 'authtodoapp-b21b9.firebasestorage.app',
-  );
+  static FirebaseOptions get android {
+    return FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY'] ?? '',
+      appId: dotenv.env['FIREBASE_APP_ID'] ?? '',
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
+      projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+    );
+  }
 }
